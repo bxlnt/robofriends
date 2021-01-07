b029fe81af01a0bbf77f3626ec47827eda61fd33
Brad Pence <brad.pence@stratagemgroup.com>
2021-01-07 16:44:14 -0700

    completed app

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+	id:1,
+	name: 'Bobby Fischer',
+	username: 'bobby',
+	email: 'chessmaster@gmail.com'
+},
+{
+	id: 2,
+	name: 'Ricky Bobby',
+	username: 'rickybobby1',
+	email: 'rickybobby@theman.biz'
+},
+{
+	id: 3,
+	name: 'Peggy Carter',
+	username: 'pcarter',
+	email: 'agent-pcarter@ssr.org'
+},
+{
+	id: 4,
+	name: 'Captian America',
+	username: 'avengerCap',
+	email: 'cap@avengers.biz'
+},
+{
+	id: 5,
+	name: 'Sweet Caroline',
+	username: 'scsong',
+	email: 'scsong@nd.artist'
+},
+{
+	id:6,
+	name: 'Han Solo',
+	username: 'bigsolo',
+	email: 'han.solo@falcon.ship'
+},
+{
+	id: 7,
+	name: 'Chewy',
+	username: 'loudbacca',
+	email: 'thechewster@falcon.ship'
+},
+{
+	id: 8,
+	name: 'Mario',
+	username: 'mario',
+	email: 'mario@nintendo.go'
+},
+{
+	id: 9,
+	name: 'Captian Underpants',
+	username: 'capu',
+	email: 'capu@book.biz'
+},
+{
+	id: 10,
+	name: 'Frank Sinatra',
+	username: 'olblueeyes',
+	email: 'frank@ratpack.org'
+}
+]
