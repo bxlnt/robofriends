c1bd5b32eb419db1fac0ad5f29f9e3fdca35c140
Brad Pence <brad.pence@stratagemgroup.com>
2021-01-07 17:21:18 -0700

    newest changes

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Card from './Card';
 
-const CardList = ({robots}) => {
+const CardList = ({ robots }) => {
+	if (true) {
+		throw new Error('NOOOOOOO')
+	}
+
 	return (
 		robots.map((user, i) => {
-				return (<Card 
-							key={i} 
-							id={robots[i].id} 
-							name={robots[i].name} 
-							email={robots[i].email} 
-						/>)
-		})	
+			return (<Card
+				key={i}
+				id={robots[i].id}
+				name={robots[i].name}
+				email={robots[i].email}
+			/>)
+		})
 	)
 }
 
